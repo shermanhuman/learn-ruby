1d66723b462d975dfc451a2e6d8cabcb72b8e84b
Sherman Boyd <sherman.boyd@gmail.com>
2017-02-01 14:57:55 -0700

    Early days

---
+++ well-grounded-rubyist/c2fi.rb
@@ -0,0 +1,6 @@
+print "Hey babe.  Enter a celsius value: "
+celsius = gets.to_i
+fahrenheit = (celsius * 9 / 5) + 32
+print "The fahrenheit equivalent is: "
+print fahrenheit
+puts "."
---
+++ well-grounded-rubyist/c2fout.rb
@@ -0,0 +1,7 @@
+print "Hey babe.  Enter a celsius value: "
+celsius = gets.to_i
+fahrenheit = (celsius * 9 / 5) + 32
+puts "Saving result to 'temp.out'."
+fh = File.new("temp.out", "w")
+fh.puts fahrenheit
+fh.close
