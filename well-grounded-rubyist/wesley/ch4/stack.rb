2ef6278d9855b66fa6f75728d74460987819a0f8
Wesley David <nonapeptide@gmail.com>
2017-11-04 21:50:32 -0700

    More work on chapter 4

---
+++ well-grounded-rubyist/wesley/ch4/stack.rb
@@ -0,0 +1,17 @@
+require_relative "stacklike.rb"
+
+class Stack
+  include Stacklike
+end
+
+s = Stack.new
+s.add_to_stack("item one")
+s.add_to_stack("item two")
+s.add_to_stack("item three")
+puts "Objects currently in the stack:"
+puts s.stack
+taken = s.take_from_stack
+puts "Removed this object:"
+puts taken
+puts "Now on stack:"
+puts s.stack
