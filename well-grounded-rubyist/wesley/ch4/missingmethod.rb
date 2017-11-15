edcf424604605b8c3f9100fd04fee9b3e703b7c1
Wesley David <nonapeptide@gmail.com>
2017-11-15 16:24:42 -0700

    Chapter 5

--- well-grounded-rubyist/wesley/ch4/missingmethod.rb
+++ well-grounded-rubyist/wesley/ch4/missingmethod.rb
@@ -1,10 +1,9 @@
 obj = Object.new
 def obj.method_missing(m, *args)
- call_name = m.to_s
- if call_name.include? "dance"
+ if m.to_s.include? "dance"
   puts "I can't dance!"
  else
-  puts "This object can't do that."
+  puts "This object cannot do that."
  end
 end # Override method_missing for obj
 
